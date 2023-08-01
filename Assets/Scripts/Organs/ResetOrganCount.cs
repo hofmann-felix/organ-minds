2b83bce3a84162e4bbada7c1da26ff8e903fc5cb
Felix Hofmann <felix.hofmann@satellytes.com>
2023-08-01 14:57:03 +0200

    reset organ index on app start

---
+++ Assets/Scripts/Organs/ResetOrganCount.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOrganCount : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("currentOrganIndex", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
