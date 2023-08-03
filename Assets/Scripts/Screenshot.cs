9ff9a9633496c5430895ada87a91674d684a7f4b
rea0907 <reasangi01@gmail.com>
2023-08-03 10:43:56 +0200

    screenshot

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -1,11 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class Screenshot : MonoBehaviour
+public class ScreenShot : MonoBehaviour
 {
-    public void OnMouseDown()
-    {
-        ScreenCapture.CaptureScreenshot("Urkunde.png");
+
+    
+    private IEnumerator Screenshot(){
+
+        yield return new WaitForEndOfFrame();
+        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        texture.ReadPixels(new Rect(0, 0, Screen.width , Screen.height), 0, 0);
+        texture.Apply();
+
+        string name = "Screenshot_EpicApp" + System.DateTime.Now.ToString("yyy-MM-dd-HH-mm-ss") + ".png";
+
+        //PC
+
+        //byte[] bytes = texture.EncodeToPNG();
+        //File.WriteAllBytes(Application.dataPath + "/../" + name, bytes);
+
+        //Mobile
+        NativeGallery.SaveImageToGallery(texture, "MyApp pictures", name);
+
+
+        Destroy(texture);
+
+    }
+
+    public void TakeScreenShot(){
+
+        StartCoroutine("Screenshot");
+
     }
 }
