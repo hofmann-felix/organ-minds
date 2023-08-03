c684db5f7169985615171908b94075db99113e89
ru42fur <ru42fur>
2023-08-03 12:15:29 +0200

    Added Room Name to Room Scene

---
+++ Assets/Scripts/Photon/UI/Rooms/ShowRoomName.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class ShowRoomName : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _roomName;
+
+    bool isSet = false;
+
+    void Update()
+    {
+        if (!isSet)
+        {
+            _roomName.text = PhotonNetwork.CurrentRoom.Name;
+            isSet = true;
+        }
+    }
+
+}
