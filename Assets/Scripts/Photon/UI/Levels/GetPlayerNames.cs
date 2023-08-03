c2e290b9b1f805e0d7195b992b63b9f8f11e437d
ru42fur <ru42fur>
2023-08-03 09:32:26 +0200

    added 2nd player name to urkunde

--- Assets/Scripts/Photon/UI/Levels/GetPlayerNames.cs
+++ Assets/Scripts/Photon/UI/Levels/GetPlayerNames.cs
@@ -12,35 +12,21 @@ public class GetPlayerNames : MonoBehaviour
 
     [SerializeField]
     private TextMeshProUGUI _player2;
-    //public Player Player { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        //SetPlayerInfo();
-        //_player1.text = player.NickName;
-        //Player playerList[] = PhotonNetwork.PlayerListOthers;
-        //player2 = playerList[0];
-        //_player2.text = player2;
-        _player1.text = PhotonNetwork.LocalPlayer.NickName;
-        _player2.text = PhotonNetwork.LocalPlayer.NickName;
 
-        //int score = 
+        //SetPlayerInfo();
+        _player1.text = PhotonNetwork.LocalPlayer.NickName;
+        //_player1.text = player.NickName;
+        foreach (Player p in PhotonNetwork.PlayerListOthers)
+        {
+            _player2.text = p.NickName;
+        }
+        
 
     }
 
-
-
-    //public void SetPlayerInfo(Player player)
-    //{
-    //Player = player;
-    //_player1.text = PhotonNetwork.player.NickName;
-    //_player2.text = PhotonNetwork.player.NickName;
-
-    //    
-    //    _player1.text = player.NickName;
-    //    _player2.text = player.NickName;
-    //}
 
 }
