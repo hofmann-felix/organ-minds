530aef7a37a4921a513966e88fca2441d7f3c122
Felix Hofmann <felix.hofmann@satellytes.com>
2023-08-03 10:53:46 +0200

    add debug logs to switch scene

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Realtime;
 using Photon.Pun;
+using System;
 
 public class SwitchScene : MonoBehaviour
 {
@@ -22,27 +23,33 @@ public class SwitchScene : MonoBehaviour
         SceneManager.LoadScene("HumanBodyTracking3D");
     }
 
-    public void QuizScene1(){
+    public void QuizScene1()
+    {
         SceneManager.LoadScene("QuizScene1");
     }
 
-     public void QuizScene2(){
+    public void QuizScene2()
+    {
         SceneManager.LoadScene("QuizScene2");
     }
 
-     public void QuizScene3(){
+    public void QuizScene3()
+    {
         SceneManager.LoadScene("QuizScene3");
     }
 
-     public void QuizScene4(){
+    public void QuizScene4()
+    {
         SceneManager.LoadScene("QuizScene4");
     }
 
-     public void QuizScene5(){
+    public void QuizScene5()
+    {
         SceneManager.LoadScene("QuizScene5");
     }
 
-     public void QuizScene6(){
+    public void QuizScene6()
+    {
         SceneManager.LoadScene("QuizScene6");
     }
 
@@ -84,32 +91,80 @@ public class SwitchScene : MonoBehaviour
 
     public void Intermediate1()
     {
-        PhotonNetwork.LoadLevel("IntermediateScene1");
+        try
+        {
+            Debug.Log("Loading: IntermediateScene1");
+            PhotonNetwork.LoadLevel("IntermediateScene1");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Fehler in switchScene:" + e);
+        }
     }
 
     public void Intermediate2()
     {
-        PhotonNetwork.LoadLevel("IntermediateScene2");
+        try
+        {
+            Debug.Log("Loading: IntermediateScene2");
+            PhotonNetwork.LoadLevel("IntermediateScene2");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Fehler in switchScene:" + e);
+        }
     }
 
     public void Intermediate3()
     {
-        PhotonNetwork.LoadLevel("IntermediateScene3");
+        try
+        {
+            Debug.Log("Loading: IntermediateScene3");
+            PhotonNetwork.LoadLevel("IntermediateScene3");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Fehler in switchScene:" + e);
+        }
     }
 
     public void Intermediate4()
     {
-        PhotonNetwork.LoadLevel("IntermediateScene4");
+        try
+        {
+            Debug.Log("Loading: IntermediateScene4");
+            PhotonNetwork.LoadLevel("IntermediateScene4");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Fehler in switchScene:" + e);
+        }
     }
 
     public void Intermediate5()
     {
-        PhotonNetwork.LoadLevel("IntermediateScene5");
+        try
+        {
+            Debug.Log("Loading: IntermediateScene5");
+            PhotonNetwork.LoadLevel("IntermediateScene5");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Fehler in switchScene:" + e);
+        }
     }
 
     public void Intermediate6()
     {
-        PhotonNetwork.LoadLevel("IntermediateScene6");
+        try
+        {
+            Debug.Log("Loading: IntermediateScene6");
+            PhotonNetwork.LoadLevel("IntermediateScene6");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Fehler in switchScene:" + e);
+        }
     }
 
     public void OrganDetectorMaster()
